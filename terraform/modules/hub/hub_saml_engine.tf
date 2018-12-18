1c7a2056345907c895fef899334a108ea877fb58
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-12-18 15:54:23 +0000

    Add initial task definition for saml-engine
    - task def
    - nonProxyHosts config
    - provision instances
    
    @adityapahuja @tlwr

--- terraform/modules/hub/hub_saml_engine.tf
+++ terraform/modules/hub/hub_saml_engine.tf
@@ -7,8 +7,7 @@ module "saml_engine_ecs_asg" {
   vpc_id           = "${aws_vpc.hub.id}"
   instance_subnets = ["${aws_subnet.internal.*.id}"]
 
-  # number_of_instances = "${var.number_of_availability_zones}"
-  number_of_instances = 0
+  number_of_instances = "${var.number_of_availability_zones}"
   domain              = "${var.domain}"
 
   additional_instance_security_group_ids = [
@@ -17,10 +16,12 @@ module "saml_engine_ecs_asg" {
 }
 
 data "template_file" "saml_engine_task_def" {
-  template = "${file("${path.module}/files/tasks/stub.json")}"
+  template = "${file("${path.module}/files/tasks/hub-saml-engine.json")}"
 
   vars {
-    app = "saml_engine"
+    image_and_tag = "${local.tools_account_ecr_url_prefix}-verify-saml-engine:latest"
+    domain        = "${var.domain}"
+    deployment    = "${var.deployment}"
   }
 }
 
@@ -33,11 +34,33 @@ module "saml_engine" {
   vpc_id                = "${aws_vpc.hub.id}"
   lb_subnets            = ["${aws_subnet.internal.*.id}"]
   task_definition       = "${data.template_file.saml_engine_task_def.rendered}"
-  container_name        = "stub"
+  container_name        = "saml-engine"
   container_port        = "8080"
   number_of_tasks       = 1
   health_check_protocol = "HTTP"
   tools_account_id      = "${var.tools_account_id}"
+  image_name                 = "verify-saml-engine"
   instance_security_group_id = "${module.saml_engine_ecs_asg.instance_sg_id}"
   certificate_arn            = "${data.aws_acm_certificate.wildcard.arn}"
+}
+
+module "saml_engine_can_connect_to_config" {
+  source = "modules/microservice_connection"
+
+  source_sg_id      = "${module.saml_engine_ecs_asg.instance_sg_id}"
+  destination_sg_id = "${module.config.lb_sg_id}"
+}
+
+module "saml_engine_can_connect_to_policy" {
+  source = "modules/microservice_connection"
+
+  source_sg_id      = "${module.saml_engine_ecs_asg.instance_sg_id}"
+  destination_sg_id = "${module.policy.lb_sg_id}"
+}
+
+module "saml_engine_can_connect_to_saml_soap_proxy" {
+  source = "modules/microservice_connection"
+
+  source_sg_id      = "${module.saml_engine_ecs_asg.instance_sg_id}"
+  destination_sg_id = "${module.saml_soap_proxy.lb_sg_id}"
 }
