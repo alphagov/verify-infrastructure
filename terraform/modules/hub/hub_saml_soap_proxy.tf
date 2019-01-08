e7cbea0eb41b92d926798664f706845f164ea296
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2019-01-08 10:17:34 +0000

    Add nginx in front of saml-soap-proxy application
    This commit adds nginx in front of saml-soap-proxy application and nginx acts as a SSL termination.
    
    Author: @adityapahuja

--- terraform/modules/hub/hub_saml_soap_proxy.tf
+++ terraform/modules/hub/hub_saml_soap_proxy.tf
@@ -17,34 +17,48 @@ module "saml_soap_proxy_ecs_asg" {
   logit_elasticsearch_url = "${var.logit_elasticsearch_url}"
 }
 
+locals {
+  saml_soap_proxy_location_blocks = <<-LOCATIONS
+  location / {
+    proxy_pass http://saml-soap-proxy:8080;
+    proxy_set_header Host saml-soap-proxy.${local.root_domain};
+  }
+  LOCATIONS
+
+  nginx_saml_soap_proxy_location_blocks_base64 = "${base64encode(local.saml_soap_proxy_location_blocks)}"
+}
+
 data "template_file" "saml_soap_proxy_task_def" {
   template = "${file("${path.module}/files/tasks/hub-saml-soap-proxy.json")}"
 
   vars {
-    image_and_tag = "${local.tools_account_ecr_url_prefix}-verify-saml-soap-proxy:latest"
-    domain        = "${local.root_domain}"
-    deployment    = "${var.deployment}"
+    image_and_tag          = "${local.tools_account_ecr_url_prefix}-verify-saml-soap-proxy:latest"
+    nginx_image_and_tag    = "${local.tools_account_ecr_url_prefix}-verify-nginx-tls:latest"
+    domain                 = "${local.root_domain}"
+    deployment             = "${var.deployment}"
+    location_blocks_base64 = "${local.nginx_saml_soap_proxy_location_blocks_base64}"
   }
 }
 
 module "saml_soap_proxy" {
   source = "modules/ecs_app"
 
-  deployment                 = "${var.deployment}"
-  cluster                    = "saml-soap-proxy"
-  domain                     = "${local.root_domain}"
-  vpc_id                     = "${aws_vpc.hub.id}"
-  lb_subnets                 = ["${aws_subnet.internal.*.id}"]
-  task_definition            = "${data.template_file.saml_soap_proxy_task_def.rendered}"
-  container_name             = "saml-soap-proxy"
-  container_port             = "8080"
-  number_of_tasks            = 1
-  health_check_protocol      = "HTTP"
-  health_check_path          = "/service-status"
-  tools_account_id           = "${var.tools_account_id}"
-  instance_security_group_id = "${module.saml_soap_proxy_ecs_asg.instance_sg_id}"
-  certificate_arn            = "${local.wildcard_cert_arn}"
-  image_name                 = "verify-saml-soap-proxy"
+  deployment                   = "${var.deployment}"
+  cluster                      = "saml-soap-proxy"
+  domain                       = "${local.root_domain}"
+  vpc_id                       = "${aws_vpc.hub.id}"
+  lb_subnets                   = ["${aws_subnet.internal.*.id}"]
+  task_definition              = "${data.template_file.saml_soap_proxy_task_def.rendered}"
+  container_name               = "nginx"
+  container_port               = "8443"
+  number_of_tasks              = 1
+  aws_lb_target_group_port     = 8443
+  aws_lb_target_group_protocol = "HTTPS"
+  health_check_path            = "/service-status"
+  tools_account_id             = "${var.tools_account_id}"
+  instance_security_group_id   = "${module.saml_soap_proxy_ecs_asg.instance_sg_id}"
+  certificate_arn              = "${local.wildcard_cert_arn}"
+  image_name                   = "verify-saml-soap-proxy"
 }
 
 module "saml_soap_proxy_can_connect_to_config" {
