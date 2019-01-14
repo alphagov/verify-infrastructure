535daca470c01906672a688353f78b380c7377c6
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-01-14 17:41:30 +0000

    add wildcard cert for mgmt.$root_domain

--- terraform/modules/hub/mgmt.tf
+++ terraform/modules/hub/mgmt.tf
@@ -74,10 +74,41 @@ resource "aws_lb_listener" "mgmt_http" {
 #   }
 # }
 
+locals {
+  mgmt_domain = "mgmt.${local.root_domain}"
+}
+
 resource "aws_route53_zone" "mgmt_domain" {
-  name = "mgmt.${local.root_domain}"
+  name = "${local.mgmt_domain}"
 
   tags {
     Deployment = "${var.deployment}"
   }
+}
+
+resource "aws_acm_certificate" "mgmt_wildcard" {
+  domain_name       = "${local.mgmt_domain}"
+  subject_alternative_names = ["*.${local.mgmt_domain}"]
+  validation_method = "DNS"
+
+  tags {
+    Deployment = "${var.deployment}"
+  }
+}
+
+resource "aws_route53_record" "mgmt_wildcard_cert_validation" {
+  name    = "${aws_acm_certificate.mgmt_wildcard.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.mgmt_wildcard.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.mgmt_domain.zone_id}"
+
+  records = [
+    "${aws_acm_certificate.mgmt_wildcard.domain_validation_options.0.resource_record_value}",
+  ]
+
+  ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "mgmt_wildcard" {
+  certificate_arn         = "${aws_acm_certificate.mgmt_wildcard.arn}"
+  validation_record_fqdns = ["${aws_route53_record.mgmt_wildcard_cert_validation.fqdn}"]
 }
