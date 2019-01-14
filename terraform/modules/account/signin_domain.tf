b8e5e8e78181f36a2bdcc970311b55d6918bdd4a
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-01-14 17:24:31 +0000

    remove old unnecessary comment

--- terraform/modules/account/signin_domain.tf
+++ terraform/modules/account/signin_domain.tf
@@ -14,7 +14,6 @@ locals {
   domain_root = "${replace(var.signin_domain, "/^www[.]/", "")}"
 }
 
-# terraform state mv wildcard_new to wildcard after we change the environment domain
 resource "aws_acm_certificate" "wildcard" {
   domain_name               = "${var.signin_domain}"
   subject_alternative_names = ["*.${local.domain_root}"]
