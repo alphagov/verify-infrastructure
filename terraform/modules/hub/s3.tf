b6d6ed7805a9d306245ef9f1bfa9ec5d4ef1ebd4
Isabell (Issy) Long <me@issyl0.co.uk>
2019-01-15 13:06:40 +0000

    Add an S3 module to configure the hub config bucket
    - The config bucket is versioned, encrypted with AES256 and private.

---
+++ terraform/modules/hub/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "deployment_config" {
+  bucket = "gds-${var.deployment}-config"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
