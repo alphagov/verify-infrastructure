3db40a9cda29463eab81a8c8eb698baf04403026
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2019-01-16 12:13:18 +0000

    WiP - Add app_asg, app_ecs and basic task definition

--- terraform/modules/hub/static_ingress.tf
+++ terraform/modules/hub/static_ingress.tf
@@ -1,3 +1,47 @@
+module "static_ingress_ecs_asg" {
+  source = "modules/ecs_asg"
+
+  ami_id              = "${data.aws_ami.ubuntu_bionic.id}"
+  deployment          = "${var.deployment}"
+  cluster             = "static-ingress"
+  vpc_id              = "${aws_vpc.hub.id}"
+  instance_subnets    = ["${aws_subnet.internal.*.id}"]
+  number_of_instances = "${var.number_of_availability_zones}"
+  use_egress_proxy    = true
+  domain              = "${local.root_domain}"
+
+  logit_api_key           = "${var.logit_api_key}"
+  logit_elasticsearch_url = "${var.logit_elasticsearch_url}"
+}
+
+data "template_file" "static_ingress_task_def" {
+  template = "${file("${path.module}/files/tasks/static-ingress.json")}"
+
+  vars {
+    image_and_tag = "${local.tools_account_ecr_url_prefix}-verify-static-ingress:latest"
+    deployment    = "${var.deployment}"
+  }
+}
+
+module "static-ingress" {
+  source = "modules/ecs_app"
+
+  deployment                 = "${var.deployment}"
+  cluster                    = "static-ingress"
+  domain                     = "${local.root_domain}"
+  vpc_id                     = "${aws_vpc.hub.id}"
+  lb_subnets                 = ["${aws_subnet.internal.*.id}"]
+  task_definition            = "${data.template_file.config_task_def.rendered}"
+  container_name             = "nginx"
+  container_port             = "8443"
+  number_of_tasks            = 1
+  health_check_path          = "/service-status"
+  tools_account_id           = "${var.tools_account_id}"
+  image_name                 = "verify-config"
+  instance_security_group_id = "${module.config_ecs_asg.instance_sg_id}"
+  certificate_arn            = "${local.wildcard_cert_arn}"
+}
+
 resource "aws_lb" "static_ingress" {
   name                             = "${var.deployment}-static-ingress"
   load_balancer_type               = "network"
