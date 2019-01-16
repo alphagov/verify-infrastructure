064786d7ba4a9c436c751de5a8bab38f44d018ac
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2019-01-16 15:56:19 +0000

    Explicitly define the ecs task definition for static-ingress
    The `ecs_app` module creates a ton of unnecessary resources (like ALBs)
    and it also injects unnecessary configuration to the instances (like JAVA_OPTS).
    
    This commit follows on the steps of the frontend app where the ecs tasks
    are defined explicitly to avoid exactly these issues.

--- terraform/modules/hub/static_ingress.tf
+++ terraform/modules/hub/static_ingress.tf
@@ -28,23 +28,43 @@ data "template_file" "static_ingress_task_def" {
   }
 }
 
-module "static-ingress" {
-  source = "modules/ecs_app"
+module "static_ingress_ecs_roles" {
+  source = "modules/ecs_iam_role_pair"
 
-  deployment                 = "${var.deployment}"
-  cluster                    = "static-ingress"
-  domain                     = "${local.root_domain}"
-  vpc_id                     = "${aws_vpc.hub.id}"
-  lb_subnets                 = ["${aws_subnet.internal.*.id}"]
-  task_definition            = "${data.template_file.config_task_def.rendered}"
-  container_name             = "nginx"
-  container_port             = "8443"
-  number_of_tasks            = 1
-  health_check_path          = "/service-status"
-  tools_account_id           = "${var.tools_account_id}"
-  image_name                 = "verify-config"
-  instance_security_group_id = "${module.config_ecs_asg.instance_sg_id}"
-  certificate_arn            = "${local.wildcard_cert_arn}"
+  deployment       = "${var.deployment}"
+  tools_account_id = "${var.tools_account_id}"
+  service_name     = "static-ingress"
+  image_name       = "verify-static-ingress"
+}
+
+resource "aws_ecs_task_definition" "static_ingress" {
+  family                = "${var.deployment}-static-ingress"
+  container_definitions = "${data.template_file.static_ingress_task_def.rendered}"
+  execution_role_arn    = "${module.static_ingress_ecs_roles.execution_role_arn}"
+}
+
+resource "aws_ecs_cluster" "static-ingress" {
+  name = "${var.deployment}-static-ingress"
+}
+
+resource "aws_security_group" "static_ingress" {
+  name        = "${var.deployment}-static-ingress"
+  description = "${var.deployment}-static-ingress"
+
+  vpc_id = "${aws_vpc.hub.id}"
+}
+
+resource "aws_ecs_service" "static_ingress" {
+  name            = "${var.deployment}-static-ingress"
+  cluster         = "${aws_ecs_cluster.static-ingress.id}"
+  task_definition = "${aws_ecs_task_definition.static_ingress.arn}"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.static_ingress.arn}"
+    container_name   = "static-ingress"
+    container_port   = "4500"
+  }
 }
 
 resource "aws_lb" "static_ingress" {
