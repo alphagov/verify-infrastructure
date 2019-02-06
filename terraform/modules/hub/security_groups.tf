31407cddcd0cd499fbcdfbef12d7e5610acc73c2
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-02-06 13:05:16 +0000

    move egress proxy sg config into egress_proxy.tf
    noop
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/modules/hub/security_groups.tf
+++ terraform/modules/hub/security_groups.tf
@@ -1,26 +1,8 @@
-resource "aws_security_group" "egress_via_proxy" {
-  name        = "${var.deployment}-egress-via-proxy"
-  description = "${var.deployment}-egress-via-proxy"
-
-  vpc_id = "${aws_vpc.hub.id}"
-}
-
 resource "aws_security_group" "scraped_by_prometheus" {
   name        = "${var.deployment}-scraped-by-prometheus"
   description = "${var.deployment}-scraped-by-prometheus"
 
   vpc_id = "${aws_vpc.hub.id}"
-}
-
-resource "aws_security_group_rule" "egress_via_proxy_egress_to_egress_proxy_lb_over_nonpriv_http" {
-  type      = "egress"
-  protocol  = "tcp"
-  from_port = 8080
-  to_port   = 8080
-
-  # source is destination for egress rules
-  source_security_group_id = "${aws_security_group.egress_proxy_lb.id}"
-  security_group_id        = "${aws_security_group.egress_via_proxy.id}"
 }
 
 module "scraped_by_prometheus_can_be_scraped_by_prometheus" {
