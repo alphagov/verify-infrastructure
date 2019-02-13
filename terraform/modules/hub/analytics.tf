37063f34889e9e683e41b4ff0ada372eccd248f1
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2019-02-13 14:00:24 +0000

    Add nginx to forward /analytics to analytics.tools...
    Co-authored-by: issyl0 <isabell.long@digital.cabinet-office.gov.uk>
    Co-authored-by: matthewcullum-gds <matthew.cullum@digital.cabinet-office.gov.uk>
    Co-authored-by: andy-paine <andy.paine@digital.cabinet-office.gov.uk>

---
+++ terraform/modules/hub/analytics.tf
@@ -0,0 +1,97 @@
+# analytics
+#
+# frontend applications are run on the ingress asg
+
+resource "aws_security_group" "analytics_task" {
+  name        = "${var.deployment}-analytics-task"
+  description = "${var.deployment}-analytics-task"
+
+  vpc_id = "${aws_vpc.hub.id}"
+}
+
+locals {
+  analytics_location_blocks = <<-LOCATIONS
+
+  set $analytics "https://analytics.tools.signin.service.gov.uk";
+  location /analytics {
+    proxy_pass $analytics/matomo.php?$args;
+  }
+
+  location /healthcheck {
+    return 200;
+  }
+
+  LOCATIONS
+
+  nginx_analytics_location_blocks_base64 = "${base64encode(local.analytics_location_blocks)}"
+}
+
+data "template_file" "analytics_task_def" {
+  template = "${file("${path.module}/files/tasks/analytics.json")}"
+
+  vars {
+    deployment             = "${var.deployment}"
+    nginx_image_identifier = "${local.tools_account_ecr_url_prefix}-verify-nginx-tls@${var.nginx_image_digest}"
+    location_blocks_base64 = "${local.nginx_analytics_location_blocks_base64}"
+  }
+}
+
+module "analytics_ecs_roles" {
+  source = "modules/ecs_iam_role_pair"
+
+  deployment       = "${var.deployment}"
+  service_name     = "analytics"
+  tools_account_id = "${var.tools_account_id}"
+}
+
+resource "aws_ecs_task_definition" "analytics" {
+  family                = "${var.deployment}-analytics"
+  container_definitions = "${data.template_file.analytics_task_def.rendered}"
+  network_mode          = "awsvpc"
+  execution_role_arn    = "${module.analytics_ecs_roles.execution_role_arn}"
+}
+
+resource "aws_security_group_rule" "analytics_task_egress_to_internet_over_http" {
+  type      = "egress"
+  protocol  = "tcp"
+  from_port = 80
+  to_port   = 80
+
+  security_group_id = "${aws_security_group.analytics_task.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "analytics_task_egress_to_internet_over_https" {
+  type      = "egress"
+  protocol  = "tcp"
+  from_port = 443
+  to_port   = 443
+
+  security_group_id = "${aws_security_group.analytics_task.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_ecs_service" "analytics" {
+  name            = "${var.deployment}-analytics"
+  cluster         = "${aws_ecs_cluster.ingress.id}"
+  task_definition = "${aws_ecs_task_definition.analytics.arn}"
+
+  desired_count                      = "${var.number_of_apps}"
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.ingress_analytics.arn}"
+    container_name   = "nginx"
+    container_port   = "8443"
+  }
+
+  network_configuration {
+    subnets = ["${aws_subnet.internal.*.id}"]
+
+    security_groups = [
+      "${aws_security_group.analytics_task.id}",
+      "${aws_security_group.can_connect_to_container_vpc_endpoint.id}",
+    ]
+  }
+}
