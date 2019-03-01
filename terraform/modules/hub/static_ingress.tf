cd6749a03f9501c5bc5195de36e03528305c78e6
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-03-01 22:20:09 +0000

    Use static-ingress-tls image
    Make NLB do TLS by using the new static-ingress-tls image
    
    See d85f0cd
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/modules/hub/static_ingress.tf
+++ terraform/modules/hub/static_ingress.tf
@@ -115,7 +115,7 @@ data "template_file" "static_ingress_https_task_def" {
   template = "${file("${path.module}/files/tasks/static-ingress.json")}"
 
   vars {
-    image_identifier = "${local.tools_account_ecr_url_prefix}-verify-static-ingress@${var.static_ingress_image_digest}"
+    image_identifier = "${local.tools_account_ecr_url_prefix}-verify-static-ingress-tls@${var.static_ingress_tls_image_digest}"
     backend          = "${var.signin_domain}"
     bind_port        = 443
     backend_port     = 443
@@ -213,7 +213,7 @@ resource "aws_lb_target_group" "static_ingress_http" {
 resource "aws_lb_target_group" "static_ingress_https" {
   name                 = "${var.deployment}-static-ingress-https"
   port                 = 443
-  protocol             = "TCP"
+  protocol             = "TLS"
   vpc_id               = "${aws_vpc.hub.id}"
   deregistration_delay = 30
 }
@@ -233,6 +233,7 @@ resource "aws_lb_listener" "static_ingress_https" {
   load_balancer_arn = "${aws_lb.static_ingress.arn}"
   protocol          = "TCP"
   port              = 443
+  certificate_arn   = "${local.wildcard_cert_arn}"
 
   default_action {
     type             = "forward"
