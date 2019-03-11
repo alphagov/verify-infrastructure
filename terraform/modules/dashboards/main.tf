0f664b58b4c707561298134725a11e3c4ad00fe6
Isabell (Issy) Long <me@issyl0.co.uk>
2019-03-11 12:19:06 +0000

    Delete the page-type alerts Grafana dashboard
    - We deleted the alerts that this dashboard was made for (Grafana alerts
      only work on graphs). They're now handled by AlertManager and
      Prometheus.

--- terraform/modules/dashboards/main.tf
+++ terraform/modules/dashboards/main.tf
@@ -6,12 +6,3 @@ data "template_file" "tickets_dashboard" {
     source     = "${var.data_source}"
   }
 }
-
-data "template_file" "pages_dashboard" {
-  template = "${file("${path.module}/pages_dashboard.json.tpl")}"
-
-  vars = {
-    deployment = "${var.deployment}"
-    source     = "${var.data_source}"
-  }
-}
--- terraform/modules/dashboards/outputs.tf
+++ terraform/modules/dashboards/outputs.tf
@@ -1,7 +1,3 @@
 output "tickets_dashboard_rendered" {
   value = "${data.template_file.tickets_dashboard.rendered}"
 }
-
-output "pages_dashboard_rendered" {
-  value = "${data.template_file.pages_dashboard.rendered}"
-}
