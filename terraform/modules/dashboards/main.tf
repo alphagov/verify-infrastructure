1a0a983f121ea2c001a533d7c97cc439586429c6
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2019-03-28 17:38:07 +0000

    remove infra tickets dashboard
    Since #199, these alerts are in prometheus

--- terraform/modules/dashboards/main.tf
+++ terraform/modules/dashboards/main.tf
@@ -7,15 +7,6 @@ data "template_file" "service_tickets_dashboard" {
   }
 }
 
-data "template_file" "infra_tickets_dashboard" {
-  template = "${file("${path.module}/infra_tickets_dashboard.json.tpl")}"
-
-  vars = {
-    deployment = "${var.deployment}"
-    source     = "${var.data_source}"
-  }
-}
-
 data "template_file" "verify_frontend_dashboard" {
   template = "${file("${path.module}/verify_frontend_dashboard.json.tpl")}"
 
--- terraform/modules/dashboards/outputs.tf
+++ terraform/modules/dashboards/outputs.tf
@@ -2,10 +2,6 @@ output "service_tickets_dashboard_rendered" {
   value = "${data.template_file.service_tickets_dashboard.rendered}"
 }
 
-output "infra_tickets_dashboard_rendered" {
-  value = "${data.template_file.infra_tickets_dashboard.rendered}"
-}
-
 output "verify_frontend_dashboard_rendered" {
   value = "${data.template_file.verify_frontend_dashboard.rendered}"
 }
