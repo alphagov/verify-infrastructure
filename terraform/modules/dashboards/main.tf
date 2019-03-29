895f146ef85f242b15cab79c2171a79da3fb0151
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2019-03-29 10:15:28 +0000

    remove frontend dashboard
    no longer used once alphagov/verify-terraform#896 is merged.

--- terraform/modules/dashboards/main.tf
+++ terraform/modules/dashboards/main.tf
@@ -6,12 +6,3 @@ data "template_file" "service_tickets_dashboard" {
     source     = "${var.data_source}"
   }
 }
-
-data "template_file" "verify_frontend_dashboard" {
-  template = "${file("${path.module}/verify_frontend_dashboard.json.tpl")}"
-
-  vars = {
-    deployment = "${var.deployment}"
-    source     = "${var.data_source}"
-  }
-}
--- terraform/modules/dashboards/outputs.tf
+++ terraform/modules/dashboards/outputs.tf
@@ -1,7 +1,3 @@
 output "service_tickets_dashboard_rendered" {
   value = "${data.template_file.service_tickets_dashboard.rendered}"
 }
-
-output "verify_frontend_dashboard_rendered" {
-  value = "${data.template_file.verify_frontend_dashboard.rendered}"
-}
