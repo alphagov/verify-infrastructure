5ee7e4657e46f7d5d990104af8fb561a9bba7775
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2019-04-01 23:28:05 +0100

    WIP: Fiddle name of manifest for more anchoring

--- paas/stubs/bootstrap-stubs.sh
+++ paas/stubs/bootstrap-stubs.sh
@@ -4,33 +4,38 @@ set -eu
 echo "Bootstrapping environment: ${ENVIRONMENT?Must have set ENVIRONMENT environment variable}"
 echo "Only allowing access to test-rp web service from: ${TEST_RP_IP_SAFELIST?Must have set TEST_RP_IP_SAFELIST}"
 echo "Only allowing access to test-rp private endpoints from: ${TEST_RP_PRIVATE_IP_SAFELIST?Must have set TEST_RP_PRIVATE_IP_SAFELIST}"
-echo "Only allowing access to test-rp-msa from: ${TEST_RP_MSA_IP_SAFELIST?Must have set TEST_RP_MSA_IP_SAFELIST}"
+echo "Only allowing access to test-rp-msa from: ${MSA_IP_SAFELIST?Must have set MSA_IP_SAFELIST}"
 echo "Creating 'dummy' apps"
 rm -rf /tmp/paas
 mkdir -p /tmp/paas
 cd /tmp/paas
 echo "Placeholder text" > index.html
-cat << MANIFEST > manifest.yml
+cat <<MANIFEST > manifest.yml
 applications:
   - name: $ENVIRONMENT-test-rp
     routes:
       - route: $ENVIRONMENT-test-rp.apps.internal
       - route: $ENVIRONMENT-test-rp.cloudapps.digital
       - route: $ENVIRONMENT-test-rp.cloudapps.digital/private
+    buildpacks:
+      - staticfile_buildpack
 
   - name: $ENVIRONMENT-test-rp-msa
     routes:
       - route: $ENVIRONMENT-test-rp-msa.apps.internal
       - route: $ENVIRONMENT-test-rp-msa.cloudapps.digital
+    buildpacks:
+      - staticfile_buildpack
 
   - name: $ENVIRONMENT-stub-idp
     routes:
       - route: $ENVIRONMENT-stub-idp.cloudapps.digital
-
+    buildpacks:
+      - staticfile_buildpack
 MANIFEST
-cf v3-create-app "$ENVIRONMENT-test-rp"
-cf v3-create-app "$ENVIRONMENT-test-rp-msa"
-#cf v3-create-app "$ENVIRONMENT-stub-idp"
+cf push "$ENVIRONMENT-test-rp"
+cf push "$ENVIRONMENT-test-rp-msa"
+#cf push "$ENVIRONMENT-stub-idp"
 
 # Allow test-rp to access MSA metadata
 cf add-network-policy "$ENVIRONMENT-test-rp" --destination-app "$ENVIRONMENT-test-rp-msa" --protocol tcp --port 8080
@@ -65,8 +70,8 @@ cf push --var app=$ENV-test-rp-private --var allowed_ips=$TEST_RP_PRIVATE_IP_SAF
 cf create-user-provided-service $SERVICE_NAME -r "https://$SERVICE_NAME.cloudapps.digital"
 cf bind-route-service cloudapps.digital --hostname $ENV-test-rp --path private $SERVICE_NAME
 
-# Set up route service for test-rp MSA access
+# Set up route service for MSA access
 SERVICE_NAME="$ENV-test-rp-msa-ip-safelist-service"
-cf push --var app=$ENV-test-rp-msa --var allowed_ips=$TEST_RP_MSA_IP_SAFELIST
+cf push --var app=$ENV-test-rp-msa --var allowed_ips=$MSA_IP_SAFELIST
 cf create-user-provided-service $SERVICE_NAME -r "https://$SERVICE_NAME.cloudapps.digital"
 cf bind-route-service cloudapps.digital --hostname $ENV-test-rp-msa $SERVICE_NAME
