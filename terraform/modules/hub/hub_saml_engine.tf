a2cd2e177a01436f84e891146ac802ca3611c68b
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-05-02 15:10:35 +0100

    EID-1422 Add splunk appender config to saml engine
    [There's a PR](https://github.com/alphagov/verify-hub/pull/268) to add a
    Splunk appender to saml-engine. This appender needs config. This is that
    config. [See this PR](https://github.com/alphagov/verify-infrastructure-config/pull/126) for the config that's stored in
    verify-infrastructure-config.

--- terraform/modules/hub/hub_saml_engine.tf
+++ terraform/modules/hub/hub_saml_engine.tf
@@ -51,6 +51,8 @@ data "template_file" "saml_engine_task_def" {
     region                 = "${data.aws_region.region.id}"
     location_blocks_base64 = "${local.nginx_saml_engine_location_blocks_base64}"
     redis_host             = "rediss://${aws_elasticache_replication_group.saml_engine_replay_cache.primary_endpoint_address}:6379"
+    splunk_url             = "${var.splunk_url}"
+    splunk_source_type     = "${var.splunk_source_type}"
   }
 }
 
