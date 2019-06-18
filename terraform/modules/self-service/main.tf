c63b57bc42b0ef8693526edcfb8f4fe6546c20a8
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2019-06-18 14:49:08 +0100

    3VTpnx6M: Configure s3 role and policy
    - Configure self-service role and policy for reading s3 buckets
    
    Co-authored-by: Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>

--- terraform/modules/self-service/main.tf
+++ terraform/modules/self-service/main.tf
@@ -1,11 +1,16 @@
 locals {
    service     = "govukverify-self-service"
    aws_region  = "eu-west-2"
+   config_metadata_bucket = "${local.service}-${var.deployment}-config-metadata"
 }
 
 resource "aws_s3_bucket" "config_metadata" {
   bucket  = "${local.service}-${var.deployment}-config-metadata"
   region  = "${local.aws_region}"
+
+  versioning {
+    enabled = true
+  }
 
   server_side_encryption_configuration {
     rule {
@@ -51,4 +56,53 @@ data "aws_iam_policy_document" "config_metadata_bucket_policy" {
 resource "aws_s3_bucket_policy" "config_metadata_policy" {
   bucket = "${aws_s3_bucket.config_metadata.id}"
   policy = "${data.aws_iam_policy_document.config_metadata_bucket_policy.json}"
+}
+
+data "aws_iam_policy_document" "can_read_from_config_metadata_bucket" {
+  statement {
+    sid    = "BucketCanBeReadFrom"
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::${local.config_metadata_bucket}"
+    ]
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+    ]
+  }
+
+  statement {
+    sid    = "ListAllBuckets"
+    effect = "Allow"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_role" "output_bucket_reader" {
+  name               = "${local.config_metadata_bucket}-reader"
+  assume_role_policy = "${data.aws_iam_policy_document.can_read_from_config_metadata_bucket.json}"
+}
+
+resource "aws_iam_policy" "can_read_output_bucket" {
+  name   = "can-read-${local.config_metadata_bucket}"
+  policy = "${data.aws_iam_policy_document.can_read_from_config_metadata_bucket.json}"
+}
+
+resource "aws_iam_policy_attachment" "output_bucket_reader_can_read" {
+  name = "${local.config_metadata_bucket}-can-read"
+
+  roles = [
+    "${aws_iam_role.output_bucket_reader.name}",
+  ]
+
+  policy_arn = "${aws_iam_policy.can_read_output_bucket.arn}"
 }
