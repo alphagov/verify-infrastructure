42a78eb826657117d648f950543178e6f03c359b
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-08-08 16:58:20 +0100

    Added local-exec to enable MFA
    The terraform provider doesn't support setting it up. So using the CLI to do that.

--- terraform/modules/self-service/cognito.tf
+++ terraform/modules/self-service/cognito.tf
@@ -61,6 +61,10 @@ resource "aws_cognito_user_pool" "user_pool" {
       max_length = 512
     }
   }
+
+  provisioner "local-exec" {
+    command = "aws cognito-idp set-user-pool-mfa-config --user-pool-id ${aws_cognito_user_pool.user_pool.id} --software-token-mfa-configuration Enabled=true --mfa-configuration OPTIONAL"
+  }
 }
 
 resource "aws_cognito_user_pool_client" "client" {
