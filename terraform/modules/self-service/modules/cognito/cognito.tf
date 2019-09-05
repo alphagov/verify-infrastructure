ba77a5164c846ae095ecc508b8fc2c91d485a427
brenetic <b@brenetic.com>
2019-09-05 13:12:27 +0100

    Output cognito module ID's for Self Service
    Now Cognito is a module we need to output these IDs in order for the ECS
    tasks to make use of them.

--- terraform/modules/self-service/modules/cognito/cognito.tf
+++ terraform/modules/self-service/modules/cognito/cognito.tf
@@ -90,3 +90,12 @@ resource "aws_cognito_user_pool_client" "client" {
   supported_identity_providers = ["COGNITO"]
   refresh_token_validity       = 1
 }
+
+
+output "user_pool_client_id" {
+  value = "${aws_cognito_user_pool_client.client.id}"
+}
+
+output "user_pool_id" {
+  value = "${aws_cognito_user_pool.user_pool.id}"
+}
