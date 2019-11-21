2a1d64b5a755d73d30be961cce44217eef36ad00
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-11-21 14:10:11 +0000

    OFCDsnoA: Output the security group ID for config load balancer
    We want the self-service app to communicate with the config service.
    This requires a new rule in the config service load balancer security group.
    This will output that ID so we can later use it to attach the new rule to it.

--- terraform/modules/hub/outputs.tf
+++ terraform/modules/hub/outputs.tf
@@ -13,3 +13,7 @@ output "can_connect_to_container_vpc_endpoint" {
 output "public_subnet_ids" {
   value = aws_subnet.ingress.*.id
 }
+
+output "config_lb_sg_id" {
+  value = module.config.lb_sg_id
+}
