a5e50e1667f44188ee60b25a65b83a38df759abe
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2020-01-07 13:05:03 +0000

    BAU: Specify CPU and Memory requirements
    According to Amazon Elastic Container Service documentation, 1 virtual CPU is equal to 1024 CPU shares. Setting CPU to 0 in a container definition means that it needs 2 CPU shares. This CPU requirement is quite low. This commit updates all container definitions to specify both CPU and memory requirements. This will ensure that each application gets a fair share of CPU and memory.
    
    Author: @adityapahuja

--- terraform/modules/hub/static_ingress.tf
+++ terraform/modules/hub/static_ingress.tf
@@ -95,6 +95,13 @@ module "static_ingress_can_connect_to_ingress_https" {
   port = 443
 }
 
+locals {
+  allocated_cpu_for_http     = 896
+  allocated_cpu_for_https    = 1024
+  allocated_memory_for_http  = 250
+  allocated_memory_for_https = 3000
+}
+
 data "template_file" "static_ingress_http_task_def" {
   template = file("${path.module}/files/tasks/static-ingress.json")
 
@@ -103,7 +110,8 @@ data "template_file" "static_ingress_http_task_def" {
     backend          = var.signin_domain
     bind_port        = 80
     backend_port     = 80
-    allocated_memory = 250
+    allocated_cpu    = local.allocated_cpu_for_http
+    allocated_memory = local.allocated_memory_for_http
   }
 }
 
@@ -115,7 +123,8 @@ data "template_file" "static_ingress_https_task_def" {
     backend          = var.signin_domain
     bind_port        = 443
     backend_port     = 443
-    allocated_memory = 3000
+    allocated_cpu    = local.allocated_cpu_for_https
+    allocated_memory = local.allocated_memory_for_https
   }
 }
 
