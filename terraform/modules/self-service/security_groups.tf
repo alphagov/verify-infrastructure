ccbbee0677021d0906428f8fa992b6ec89dce755
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2020-11-03 09:19:15 +0000

    BAU: Upgrade self-serve to tf 0.13.5
    The state file was accidentally bumped from 0.12.7 to 0.13.5. This is
    the work to actually implement that upgrade.

--- terraform/modules/self-service/security_groups.tf
+++ terraform/modules/self-service/security_groups.tf
@@ -78,9 +78,9 @@ resource "aws_security_group" "egress_to_db" {
   vpc_id      = data.terraform_remote_state.hub.outputs.vpc_id
 
   egress {
-    from_port = 5432
-    to_port   = 5432
-    protocol  = "tcp"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
