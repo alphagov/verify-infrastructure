d9e743d7ceabe30bd07a3c1b63b8b3635180de7c
Azeez <abdul.azeez@digital.cabinet-office.gov.uk>
2021-08-25 16:52:42 +0100

    added more s3 actions

--- terraform/modules/hub/hkr.tf
+++ terraform/modules/hub/hkr.tf
@@ -31,7 +31,9 @@ data "aws_iam_policy_document" "hkr_sam_bucket_policy" {
     }
     actions = [
       "s3:GetObject",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:ListBucket"
     ]
     resources = [
       "${aws_s3_bucket.hkr_sam.arn}", 
