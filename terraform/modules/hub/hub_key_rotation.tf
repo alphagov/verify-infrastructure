e72b29da2142f087b028f9464eae41e8ad4880f8
Azeez <abdul.azeez@digital.cabinet-office.gov.uk>
2021-09-28 09:45:13 +0100

    HUB-1067: createg internal lb and target group for hkr lambda to talk to prometheus internally

--- terraform/modules/hub/hub_key_rotation.tf
+++ terraform/modules/hub/hub_key_rotation.tf
@@ -101,3 +101,100 @@ resource "aws_ecr_lifecycle_policy" "hkr_lifecycle_policy" {
   }
 EOF
 }
+
+resource "aws_security_group" "sg_hkr_lambda_prom_internal" {
+  name        = "${var.deployment}-mgmt-internal-lb"
+  description = "${var.deployment}-mgmt-internal-lb"
+
+  vpc_id = aws_vpc.hub.id
+}
+
+resource "aws_security_group" "sg_hkr_lambda" {
+  name        = "${var.deployment}-hkr-lambda"
+  description = "${var.deployment}-hkr-lambda"
+
+  vpc_id = aws_vpc.hub.id
+}
+
+module "lambda_prom_lb_can_talk_to_prometheus" {
+  source = "./modules/microservice_connection"
+
+  source_sg_id      = aws_security_group.sg_hkr_lambda_prom_internal.id
+  destination_sg_id = aws_security_group.prometheus.id
+
+  port = 9090
+}
+
+resource "aws_security_group_rule" "sg_hkr_lambda_ingress" {
+  type      = "ingress"
+  from_port = "80"
+  to_port   = "80"
+  protocol  = "tcp"
+
+  source_security_group_id = aws_security_group.sg_hkr_lambda.id
+  security_group_id        = aws_security_group.sg_hkr_lambda_prom_internal.id
+}
+resource "aws_lb" "hkr_lambda_prom_internal_lb" {
+  name               = "${var.deployment}-mgmt-internal"
+  internal           = true
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.sg_hkr_lambda_prom_internal.id]
+  subnets         = aws_subnet.internal.*.id
+
+  tags = {
+    Deployment = var.deployment
+  }
+}
+
+resource "aws_lb_listener" "lambda_prom_internal_http" {
+  load_balancer_arn = aws_lb.hkr_lambda_prom_internal_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prometheus_internal.arn
+  }
+}
+
+resource "aws_lb_target_group" "prometheus_internal" {
+
+  name     = "${var.deployment}-prometheus-internal"
+  port     = 9090
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.hub.id
+
+  health_check {
+    path     = "/metrics"
+    protocol = "HTTP"
+    interval = 10
+    timeout  = 5
+  }
+}
+
+resource "aws_lb_target_group_attachment" "prometheus_internal" {
+  count = var.number_of_prometheus_apps
+
+  target_group_arn = aws_lb_target_group.prometheus_internal.arn
+  target_id        = element(aws_instance.prometheus.*.id, count.index)
+  port             = 9090
+}
+
+resource "aws_lb_listener_rule" "prometheus_http" {
+  count        = var.number_of_prometheus_apps
+  listener_arn = aws_lb_listener.lambda_prom_internal_http.arn
+  priority     = 100 + count.index
+
+  action {
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.prometheus_internal.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
